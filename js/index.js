0f0baeb96e3c2fe8aeea61aa8e40e620a794f61a
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2022-11-30 17:22:07 +0300

    add classes

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 const addItemHtml = (book, index) => {
   const child = `
-  <hr>
-  <p>Title: ${book.title}</p>
-  <p>Author: ${book.author}</p>
-  <button id="removeBtn${index}" onclick="removeBook(${index})"> Remove</button>
-  <hr>`;
+  <section class="book">
+    <div id="col1">
+      <p>Title: ${book.title}</p>
+      <p>Author: ${book.author}</p>
+    </div>
+    <button id="removeBtn${index}" onclick="remove(${index})" > Remove</button>
+  </section>`;
   const parent = document.getElementById('listBooks');
   parent.innerHTML += child;
 };
@@ -15,7 +17,32 @@ const clearListDiv = () => {
   parent.innerHTML = child;
 };
 
-const listBooks = () => {
+class Book {
+  constructor(title,author){
+    this.title = title
+    this.author = author
+  }
+}
+
+class BookStorage {
+  addBook = (newBook) => {
+    let books = JSON.parse(localStorage.getItem('allEntries'));
+    if (books == null) books = [];
+    localStorage.setItem('entry', JSON.stringify(newBook));
+    books.push(newBook);
+    localStorage.setItem('allEntries', JSON.stringify(books));
+  }
+
+  removeBook = (index) => {
+      const books = JSON.parse(localStorage.getItem('allEntries'));
+      books.splice(index, 1);
+      localStorage.setItem('allEntries', JSON.stringify(books));
+      listBooks();
+  }
+}
+
+
+listBooks = () => {
   clearListDiv();
   let books = JSON.parse(localStorage.getItem('allEntries'));
   if (books == null) books = [];
@@ -24,36 +51,22 @@ const listBooks = () => {
   }
 };
 
-class Book {
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
-  }
-}
-
 // add books
 const addBtn = document.getElementById('addBtn');
 addBtn.addEventListener('click', () => {
   const newTitle = document.getElementById('addTitleInput').value;
   const newAuthor = document.getElementById('addAuthorInput').value;
   const newBook = new Book(newTitle, newAuthor);
-
-  let books = JSON.parse(localStorage.getItem('allEntries'));
-  if (books == null) books = [];
-  localStorage.setItem('entry', JSON.stringify(newBook));
-  books.push(newBook);
-  localStorage.setItem('allEntries', JSON.stringify(books));
-  listBooks();
+  const storage = new BookStorage
+  storage.addBook(newBook)
+  listBooks()
 });
 
 // remove books
-const removeBook = (index) => {
-  const books = JSON.parse(localStorage.getItem('allEntries'));
-  books.splice(index, 1);
-  localStorage.setItem('allEntries', JSON.stringify(books));
+const remove = (index) => {
+  const storage = new BookStorage;
+  storage.removeBook(index);
   listBooks();
 };
 
 listBooks();
-
-removeBook();
